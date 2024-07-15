6e4428884d74045dd8b4becbb35541ee7e593e57
JeongSH1 <jyt001532@gmail.com>
2024-07-15 13:31:03 +0900

    sub: Clova Summary API 호출 함수 생성

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import os
 from datetime import datetime, timedelta
 
@@ -15,17 +16,40 @@ NAVER_API_HEADERS = {
     'X-Naver-Client-Secret': os.getenv('NAVER_API_CLIENT_SECRET'),
 }
 
+CLOVA_API_HEADERS = {
+    "X-NCP-APIGW-API-KEY-ID": os.getenv('CLOVA_API_CLIENT_ID'),
+    "X-NCP-APIGW-API-KEY": os.getenv('CLOVA_API_CLIENT_SECRET'),
+    "Content-Type": "application/json"
+}
+
+CLOVA_SUMMARY_API_ENDPOINT = "https://naveropenapi.apigw.ntruss.com/text-summary/v1/summarize"
+
 
 def get_naver_news(query, display, start=1, sort='date'):
     naver_news_api_url = f'https://openapi.naver.com/v1/search/news.json?query={query}&display={display}&start={start}&sort={sort}'
     return requests.get(naver_news_api_url, headers=NAVER_API_HEADERS)
+
+
+def get_clova_summary_result(content, tone=0, summary_count=1):
+    data = {
+        "document": {
+            "content": content
+        },
+        "option": {
+            "language": "ko",
+            "model": "general",
+            "tone": tone,
+            "summaryCount": summary_count
+        }
+    }
+    return requests.post(CLOVA_SUMMARY_API_ENDPOINT, headers=CLOVA_API_HEADERS, data=json.dumps(data))
+
 
 app = FastAPI()
 
 
 @app.get("/timeline")
 def get_timeline_preview(q: str):
-
     # 각 타임라인에 들어갈 아이템
     class TimelineDataItem(BaseModel):
         title: str
@@ -37,7 +61,8 @@ def get_timeline_preview(q: str):
 
     news_items = naver_news_response.json().get('items', [])
 
-    news_items = [news_item for news_item in news_items if news_item['link'].startswith('https://n.news.naver.com/mnews/article')]
+    news_items = [news_item for news_item in news_items if
+                  news_item['link'].startswith('https://n.news.naver.com/mnews/article')]
 
     for news_item in news_items:
         timeline_data_item = TimelineDataItem(
@@ -49,13 +74,17 @@ def get_timeline_preview(q: str):
         timeline_data[date_parts].append(timeline_data_item)
     return timeline_data
 
+
 @app.get("/today-issue-summary")
 def get_today_issue_summary(q: str):
     naver_news_response = get_naver_news(q, display=100, sort='date')
     news_items = naver_news_response.json().get('items', [])
-    news_items = [news_item for news_item in news_items if datetime.strptime(news_item['pubDate'], "%a, %d %b %Y %H:%M:%S %z").date() == datetime.now().date()]
+    news_items = [news_item for news_item in news_items if
+                  datetime.strptime(news_item['pubDate'], "%a, %d %b %Y %H:%M:%S %z").date() == datetime.now().date()]
     target_summary_data = ""
 
+    for news_item in news_items:
+        target_summary_data += news_item['title'] + '\n'
 
 
 uvicorn.run(app, host='0.0.0.0', port=8000)
