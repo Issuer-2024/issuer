aec9259dd5c1f967b9d66efb9d7cfa67e6c8a3b4
JeongSH1 <jyt001532@gmail.com>
2024-07-15 11:38:33 +0900

    sub: timeline data item dto 생성

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import requests
 import uvicorn
 from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
+from pydantic import BaseModel
 
 load_dotenv()
 
@@ -21,15 +22,18 @@ app = FastAPI()
 @app.get("/timeline")
 def get_timeline_preview(q: str):
 
+    # 각 타임라인에 들어갈 아이템
+    class TimelineDataItem(BaseModel):
+        title: str
+        link: str
+        date: str
+
     def get_naver_news(query, display, start=1, sort='date'):
         naver_news_api_url = f'https://openapi.naver.com/v1/search/news.json?query={query}&display={display}&start={start}&sort={sort}'
         return requests.get(naver_news_api_url, headers=NAVER_API_HEADERS)
 
-    main_timeline_data = {(datetime.today() - timedelta(days=i)).strftime('%Y-%m-%d'): [] for i in range(7)}
 
-    naver_news_response = get_naver_news(q, display=100)
-    if naver_news_response.status_code is not 200:
-        raise HTTPException(status_code=500)
+    naver_news_response = get_naver_news(q, display=100, sort='sim')
 
 
 
