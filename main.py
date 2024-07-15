be12b05baf7225ef921e8ae0914e4aec6a3a03e3
JeongSH1 <jyt001532@gmail.com>
2024-07-15 11:59:31 +0900

    sub: timeline date 반환 로직 구현(100개의 뉴스를 일자별로 분리해서 반환)

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import uvicorn
 from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
 from pydantic import BaseModel
+from collections import defaultdict
 
 load_dotenv()
 
@@ -26,14 +27,26 @@ def get_timeline_preview(q: str):
     class TimelineDataItem(BaseModel):
         title: str
         link: str
-        date: str
+        pub_date: str
 
     def get_naver_news(query, display, start=1, sort='date'):
         naver_news_api_url = f'https://openapi.naver.com/v1/search/news.json?query={query}&display={display}&start={start}&sort={sort}'
         return requests.get(naver_news_api_url, headers=NAVER_API_HEADERS)
 
-
+    timeline_data = defaultdict(list)
     naver_news_response = get_naver_news(q, display=100, sort='sim')
+
+    news_items = naver_news_response.json().get('items', [])
+
+    for news_item in news_items:
+        timeline_data_item = TimelineDataItem(
+            title=news_item['title'],
+            link=news_item['link'],
+            pub_date=news_item['pubDate'],
+        )
+        date_parts = ' '.join(timeline_data_item.pub_date.split(' ')[:3])
+        timeline_data[date_parts].append(timeline_data_item)
+    return timeline_data
 
 
 
