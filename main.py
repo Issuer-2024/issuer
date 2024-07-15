eb25e2eb832c9f4cdef4b334fd76582814c3a135
JeongSH1 <jyt001532@gmail.com>
2024-07-15 11:13:28 +0900

    sub: naver news API를 호출하는 함수 생성

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+from datetime import datetime, timedelta
+
 from fastapi import FastAPI
 import uvicorn
+import requests
 
 app = FastAPI()
 
+
 @app.get("/timeline")
 def get_timeline_preview(q: str):
+
+    def get_naver_news(query, display, start, sort):
+        naver_news_api_url = f'https://openapi.naver.com/v1/search/news.json?query={query}&display={display}&start={start}&sort={sort}'
+        return requests.get(naver_news_api_url, headers=naver_news_api_headers)
+
     pass
+
 
 
 uvicorn.run(app, host='0.0.0.0', port=8000)
