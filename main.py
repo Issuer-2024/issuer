5079aa6285eb70f3cb344374eb11911020ed38ad
JeongSH1 <jyt001532@gmail.com>
2024-07-16 12:25:28 +0900

    sub: chat completion 실행 class 생성

--- main.py
+++ main.py
@@ -34,6 +34,33 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+class CompletionExecutor:
+    def __init__(self, host, api_key, api_key_primary_val, request_id):
+        self._host = host
+        self._api_key = api_key
+        self._api_key_primary_val = api_key_primary_val
+        self._request_id = request_id
+
+    def execute(self, completion_request):
+        headers = {
+            'X-NCP-CLOVASTUDIO-API-KEY': self._api_key,
+            'X-NCP-APIGW-API-KEY': self._api_key_primary_val,
+            'X-NCP-CLOVASTUDIO-REQUEST-ID': self._request_id,
+            'Content-Type': 'application/json; charset=utf-8',
+            'Accept': 'application/json'
+        }
+
+        response = requests.post(self._host + '/testapp/v1/chat-completions/HCX-DASH-001',
+                                 headers=headers, json=completion_request)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            response.raise_for_status()
+
+
+
+
 NAVER_API_HEADERS = {
     'X-Naver-Client-Id': os.getenv('NAVER_API_CLIENT_ID'),
     'X-Naver-Client-Secret': os.getenv('NAVER_API_CLIENT_SECRET'),
