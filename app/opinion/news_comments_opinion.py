f4e44bf651f4855491c47069a4fc3c35045c3ba1
JeongSH1 <jyt001532@gmail.com>
2024-07-23 17:07:14 +0900

    enhancement: opinion 호출 시 API를 활용한 댓글 수집으로 변경

--- app/opinion/news_comments_opinion.py
+++ app/opinion/news_comments_opinion.py
@@ -1,16 +1,35 @@
-from fastapi import HTTPException
+import requests
+from bs4 import BeautifulSoup
 
-from app.request_external_api import RequestSuggestions, RequestNews
-from app.util import NewsCommentsCrawler, StringUtil, CompletionExecutor
+from app.request_external_api import RequestSuggestions, RequestNews, RequestNewsComments
+from app.util import StringUtil, CompletionExecutor
 
 import os
 
+def get_news_title(url):
+    # 웹 페이지의 HTML 가져오기
+    response = requests.get(url)
+    html_content = response.text
+
+    # BeautifulSoup 객체 생성
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # 뉴스 제목 요소 찾기
+    title_element = soup.find('h2', {'class': 'media_end_head_headline'})
+
+    # 제목 가져오기
+    if title_element:
+        title = title_element.get_text()
+        return title
+    else:
+        return None
 
 def get_news_comments_opinion(q: str):
     comment_sentiment_data = []
 
     suggestions = RequestSuggestions.get_suggestions(q)
     news_link = []
+    news_title = []
     for suggestion in suggestions[:1]:
         naver_news_response = RequestNews.get_naver_news(suggestion, display=10, sort='sim')
 
@@ -19,11 +38,11 @@ def get_news_comments_opinion(q: str):
                       news_item['link'].startswith('https://n.news.naver.com/mnews/article')]
         for news_item in news_items:
             news_link.append(news_item['link'])
+            news_title.append(get_news_title(news_item['link']))
     raw_data = []
-    news_comments_crawler = NewsCommentsCrawler()
 
-    for link in news_link:
-        tmp = news_comments_crawler.parse(StringUtil.convert_news_url_to_comment_url(link))
+    for url in news_link:
+        tmp = RequestNewsComments.get_news_comments(url)
         if tmp:
             raw_data.append(tmp)
 
@@ -34,10 +53,15 @@ def get_news_comments_opinion(q: str):
         request_id=os.getenv("CLOVA_CHAT_COMPLETION_REQUEST_ID")
     )
 
-    for item in raw_data:
+    for items, title in zip(raw_data, news_title):
+        trend_score = 0
+        contents = []
+        for item in items:
+            contents.append(item['contents'])
+            trend_score += item['sympathy_count'] + item['antipathy_count'] + item['reply_count']
         preset_text = [{"role": "system",
                         "content": "댓글을 분석하는 AI 어시스턴트 입니다.\n출력 형식: 다음 입력에 대해 3줄로 명확하게 사용자 여론을 표현해주세요.\n\n"},
-                       {"role": "user", "content": f"{item['댓글']}"}]
+                       {"role": "user", "content": f"{contents}"}]
 
         request_data = {
             'messages': preset_text,
@@ -51,6 +75,6 @@ def get_news_comments_opinion(q: str):
             'seed': 0
         }
         result = completion_executor.execute(request_data)
-        comment_sentiment_data.append({'title': item['제목'], "result": result, "ratio": item['트랜드 수치']})
+        comment_sentiment_data.append({'title': title, "result": result, "ratio": trend_score})
         comment_sentiment_data.sort(key=lambda x: x['ratio'], reverse=True)
     return comment_sentiment_data
--- app/request_external_api/request_news_comments.py
+++ app/request_external_api/request_news_comments.py
@@ -12,26 +12,33 @@ header = {
 }
 
 
+def parse_url(url):
+    match = re.search(r'/article/(\d+)/(\d+)', url)
+    if match:
+        media_id = match.group(1)
+        article_id = match.group(2)
+        return media_id, article_id
+
+    else:
+        return None, None
+
+
 class RequestNewsComments:
 
-    def _parse_url(self, url):
-        match = re.search(r'/article/(\d+)/(\d+)', url)
-        if match:
-            media_id = match.group(1)
-            article_id = match.group(2)
-            return media_id, article_id
-
-        else:
-            return None, None
-
-    def get_news_comments(self, url, num=5):
-        media_id, article_id = self._parse_url(url)
+    @staticmethod
+    def get_news_comments(url, num=5):
+        media_id, article_id = parse_url(url)
         if not media_id or not article_id:
             return None
-
-        url = f"https://apis.naver.com/commentBox/cbox/web_naver_list_jsonp.json?ticket=news&pool=cbox5&_callback=&lang=ko&country=KR&objectId=news{media_id}%2C{article_id}&pageSize={num}"
-        html = requests.get(url, headers=header)
-        comment_text = json.loads(html.text.replace('_callback(', '')[:-2])
-        comments = [comment_info['contents'] for comment_info in comment_text['result']['commentList']]
-
-        return comments
+        try:
+            url = f"https://apis.naver.com/commentBox/cbox/web_naver_list_jsonp.json?ticket=news&pool=cbox5&_callback=&lang=ko&country=KR&objectId=news{media_id}%2C{article_id}&pageSize={num}"
+            html = requests.get(url, headers=header)
+            comment_text = json.loads(html.text.replace('_callback(', '')[:-2])
+            comments = [{'contents': comment_info['contents'],
+                         'sympathy_count': comment_info['sympathyCount'],
+                         'antipathy_count': comment_info['antipathyCount'],
+                         'reply_count': comment_info['replyAllCount']}
+                        for comment_info in comment_text['result']['commentList']]
+            return comments
+        except Exception as e:
+            return None
