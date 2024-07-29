18ee1fcc4f20a0c379d73fdabef2a469aafc3510
JeongSH1 <jyt001532@gmail.com>
2024-07-29 22:12:23 +0900

    templates: header 생성

--- main.py
+++ main.py
@@ -3,25 +3,23 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
-
-from app.main_search import get_keyword_suggestion
 from app.opinion import get_news_comments_opinion
 from app.report import get_keyword_trend_variation, get_suggestions_trend_data
 from app.report.toady_issue_summary import get_today_issue_summary
 from app.request_external_api import get_google_trend_daily_rank
-from app.timeline.get_timeline import get_timeline
 from app.timeline.get_timeline import get_timeline_v2
 
 load_dotenv()
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+templates_v1 = Jinja2Templates(directory="templates/v1")
+templates_v2 = Jinja2Templates(directory="templates/v2")
 
 
 @app.get("/")
 async def render_main(request: Request):
-    return templates.TemplateResponse(
+    return templates_v1.TemplateResponse(
         request=request, name="index.html", context={
             "keyword_suggestions": get_google_trend_daily_rank()
         }
@@ -30,7 +28,7 @@ async def render_main(request: Request):
 
 @app.get("/report")
 def render_report(q: str, request: Request):
-    return templates.TemplateResponse(
+    return templates_v1.TemplateResponse(
         request=request, name="report.html", context={"issue_summary": get_today_issue_summary(q),
                                                       "trend_variation": get_keyword_trend_variation(q),
                                                       "suggestion_trend_data": get_suggestions_trend_data(q)
@@ -40,15 +38,24 @@ def render_report(q: str, request: Request):
 
 @app.get("/opinion")
 def render_opinion(q: str, request: Request):
-    return templates.TemplateResponse(
+    return templates_v1.TemplateResponse(
         request=request, name="opinion.html", context={"opinions": get_news_comments_opinion(q)}
     )
 
 
 @app.get("/timeline")
 def render_timeline(q: str, request: Request):
-    return templates.TemplateResponse(
+    return templates_v1.TemplateResponse(
         request=request, name="timeline.html", context={"timeline": get_timeline_v2(q)}
+    )
+
+
+@app.get("/test/")
+async def render_main_v2(request: Request):
+    return templates_v2.TemplateResponse(
+        request=request, name="index.html", context={
+            "keyword_suggestions": get_google_trend_daily_rank()
+        }
     )
 
 
