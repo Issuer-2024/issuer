6491d6ffb24668bd7b3bd9d3b606ed589a8f5e41
JeongSH1 <jyt001532@gmail.com>
2024-07-30 16:19:13 +0900

    sub: 문장을 임베딩하는 모듈 생성

---
+++ app/v2/external_request/request_embeding.py
@@ -0,0 +1,42 @@
+import json
+import http.client
+import os
+
+
+class CompletionExecutor:
+    def __init__(self, host, api_key, api_key_primary_val, request_id):
+        self._host = host
+        self._api_key = api_key
+        self._api_key_primary_val = api_key_primary_val
+        self._request_id = request_id
+
+    def _send_request(self, completion_request):
+        headers = {
+            'Content-Type': 'application/json; charset=utf-8',
+            'X-NCP-CLOVASTUDIO-API-KEY': self._api_key,
+            'X-NCP-APIGW-API-KEY': self._api_key_primary_val,
+            'X-NCP-CLOVASTUDIO-REQUEST-ID': self._request_id
+        }
+
+        conn = http.client.HTTPSConnection(self._host)
+        conn.request('POST', '/testapp/v1/api-tools/embedding/clir-sts-dolphin/4b9c8110bed04f15ab171c03233e424a', json.dumps(completion_request), headers)
+        response = conn.getresponse()
+        result = json.loads(response.read().decode(encoding='utf-8'))
+        conn.close()
+        return result
+
+    def execute(self, completion_request):
+        res = self._send_request(completion_request)
+        if res['status']['code'] == '20000':
+            return res['result']['embedding']
+        else:
+            return 'Error'
+
+
+if __name__ == '__main__':
+    completion_executor = CompletionExecutor(
+        host='clovastudio.apigw.ntruss.com',
+        api_key=os.getenv("CLOVA_EMBEDDING_CLIENT_KEY"),
+        api_key_primary_val = os.getenv("CLOVA_EMBEDDING_CLIENT_KEY_PRIMARY_VAR"),
+        request_id=os.getenv("CLOVA_EMBEDDING_REQUEST_ID")
+    )
